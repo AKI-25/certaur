57c934618184aea570897ff2cdd646deb7d06a2b
Abdelkefi ismail <ismail.abdelkefi@wecraft.tn>
2024-09-14 17:59:04 +0100

    added clean up and formating for webhook files

--- api/v1/certificate_webhook.go
+++ api/v1/certificate_webhook.go
@@ -32,8 +32,8 @@ import (
 )
 
 var (
-    dnsNameRegex  = `^(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$`
-    validityRegex = `^\d+d$`
+	dnsNameRegex  = `^(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$`
+	validityRegex = `^\d+d$`
 )
 
 // log is for logging in this package.
@@ -69,9 +69,7 @@ func (r *Certificate) defaultSecretName() {
 		r.Spec.SecretRef.Name = fmt.Sprintf("%s-secret", r.Name)
 	}
 }
-// TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
-// NOTE: The 'path' attribute must follow a specific pattern and should not be modified directly here.
-// Modifying the path for an invalid path can cause API server errors; failing to locate the webhook.
+
 // +kubebuilder:webhook:path=/validate-certs-k8c-io-v1-certificate,mutating=false,failurePolicy=fail,sideEffects=None,groups=certs.k8c.io,resources=certificates,verbs=create;update,versions=v1,name=vcertificate.kb.io,admissionReviewVersions=v1
 
 var _ webhook.Validator = &Certificate{}
@@ -82,17 +80,17 @@ func (r *Certificate) ValidateCreate() (admission.Warnings, error) {
 
 	var allErrs []string
 
-    if err := r.validateDNSName(); err != nil {
-        allErrs = append(allErrs, err.Error())
-    }
-    if err := r.validateValidity(); err != nil {
-        allErrs = append(allErrs, err.Error())
-    }
+	if err := r.validateDNSName(); err != nil {
+		allErrs = append(allErrs, err.Error())
+	}
+	if err := r.validateValidity(); err != nil {
+		allErrs = append(allErrs, err.Error())
+	}
 
-    if len(allErrs) == 0 {
-        return nil, nil
-    }
-    return allErrs, nil
+	if len(allErrs) == 0 {
+		return nil, nil
+	}
+	return allErrs, nil
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
@@ -105,33 +103,30 @@ func (r *Certificate) ValidateUpdate(old runtime.Object) (admission.Warnings, er
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
 func (r *Certificate) ValidateDelete() (admission.Warnings, error) {
-	certificatelog.Info("validate delete", "name", r.Name)
-
-	// TODO(user): fill in your validation logic upon object deletion.
 	return nil, nil
 }
 
 func (r *Certificate) validateDNSName() error {
-    match, _ := regexp.MatchString(dnsNameRegex, r.Spec.DnsName)
-    if !match {
-        return field.Invalid(field.NewPath("spec").Child("dnsName"), r.Spec.DnsName, "invalid DNS name")
-    }
-    return nil
+	match, _ := regexp.MatchString(dnsNameRegex, r.Spec.DnsName)
+	if !match {
+		return field.Invalid(field.NewPath("spec").Child("dnsName"), r.Spec.DnsName, "invalid DNS name")
+	}
+	return nil
 }
 
 // checks that validity is in the correct format and range.
 func (r *Certificate) validateValidity() error {
-    match, _ := regexp.MatchString(validityRegex, r.Spec.Validity)
-    if !match {
-        return errors.New("invalid format, must be a positive integer followed by 'd'")  
-    }
+	match, _ := regexp.MatchString(validityRegex, r.Spec.Validity)
+	if !match {
+		return errors.New("invalid format, must be a positive integer followed by 'd'")
+	}
 
-    // Extract the integer part of validity and check the range (0 - 1825)
-    daysStr := strings.TrimSuffix(r.Spec.Validity, "d")
-    days, err := strconv.Atoi(daysStr)
-    if err != nil || days < 1 || days > 1825 {
+	// Extract the integer part of validity and check the range (0 - 1825)
+	daysStr := strings.TrimSuffix(r.Spec.Validity, "d")
+	days, err := strconv.Atoi(daysStr)
+	if err != nil || days < 1 || days > 1825 {
 		return errors.New("validity must be between 1 and 1825 days")
-    }
+	}
 
-    return nil
+	return nil
 }
